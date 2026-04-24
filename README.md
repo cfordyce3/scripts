d2bad5928ff2b46bd5d00a56501811f1f80ada42
Chas <chasmo123@gmail.com>
2026-04-24 11:44:58 -0700

    initial commit; added audio output toggler

---
+++ README.md
@@ -0,0 +1,4 @@
+# All of my custom scripts
+
+### sysutils
+* toggle-audio-output.sh : toggles audio sink (pipewire output) (works only if there are two enabled sinks)
---
+++ sysutils/toggle-audio-output.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# A simple script to toggle my audio output between my speakers and my headphones
+
+# Get default sink (so we know which to toggle away from)
+DEFAULT_SINK=$(pactl get-default-sink)
+echo $DEFAULT_SINK
+
+# Get all sinks
+sinks=$(pactl list sinks short | grep -o "alsa[A-Za-z0-9._-]*") 
+
+# Iterate through sinks
+for sink in $sinks
+do 
+  if [ "$sink" == "$DEFAULT_SINK" ]; then
+    current_sink=$sink
+  else
+    inactive_sink=$sink
+  fi
+done
+
+# Set inactive sink as active
+pactl set-default-sink $inactive_sink
